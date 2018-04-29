eb3dd53c64ae30a9b8f314cb3e87d9252cb411cf
YSRKEN <ysr.ken@gmail.com>
2018-04-29 14:45:21 +0900

    とりあえずベタ打ちして、遠征情報を読み込み表示できるかを確かめる

--- app.js
+++ app.js
@@ -1,22 +1,71 @@
-var Greeter = (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+var Expedition = (function () {
+    function Expedition(areaName, name, time, color1, color2) {
+        this._areaName = areaName;
+        this._name = name;
+        this._time = time;
+        this._color = [color1, color2];
     }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
-    };
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
-    };
-    return Greeter;
+    Object.defineProperty(Expedition.prototype, "areaName", {
+        get: function () { return this._areaName; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Expedition.prototype, "name", {
+        get: function () { return this._name; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Expedition.prototype, "time", {
+        get: function () { return this._time; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Expedition.prototype, "color", {
+        get: function () { return this._color; },
+        enumerable: true,
+        configurable: true
+    });
+    return Expedition;
 }());
+;
+var ExpeditionTask = (function () {
+    function ExpeditionTask(expedition, timing, fleet_index) {
+        this._expedition = expedition;
+        this._timing = timing;
+        this._fleetIndex = fleet_index;
+    }
+    Object.defineProperty(ExpeditionTask.prototype, "expedition", {
+        get: function () { return this._expedition; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(ExpeditionTask.prototype, "timing", {
+        get: function () { return this._timing; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(ExpeditionTask.prototype, "fleetIndex", {
+        get: function () { return this._fleetIndex; },
+        enumerable: true,
+        configurable: true
+    });
+    return ExpeditionTask;
+}());
+;
 window.onload = function () {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    var exp1 = new Expedition("鎮守府海域", "長距離練習航海", 30, 1, 4);
+    var exp2 = new Expedition("鎮守府海域", "海上護衛任務", 90, 0, 1);
+    var exp3 = new Expedition("鎮守府海域", "防空射撃演習", 40, 3, 3);
+    var expList = [exp1, exp2, exp3];
+    var expTask1 = new ExpeditionTask(expList[1], 90, 3);
+    var expTask2 = new ExpeditionTask(expList[1], 200, 4);
+    var expTaskList = [expTask1, expTask2];
+    var element = document.getElementById('taskList');
+    for (var i = 0; i < expTaskList.length; ++i) {
+        var expTask = expTaskList[i];
+        element.innerText += "・" + expTask.expedition.areaName + "-";
+        element.innerText += expTask.expedition.name + "　";
+        element.innerText += "タイミング：" + expTask.timing + "　";
+        element.innerText += "第" + expTask.fleetIndex + "艦隊\n";
+    }
 };
--- app.ts
+++ app.ts
@@ -1,28 +1,97 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+﻿/**
+ * 遠征情報を表すクラス
+ */
+class Expedition {
+    /**
+     * 遠征海域名
+     */
+    private _areaName: string;
+    get areaName(): string { return this._areaName; }
+    /**
+     * 遠征名
+     */
+    private _name: string;
+    get name(): string { return this._name; }
+    /**
+     * 遠征時間
+     */
+    private _time: number;
+    get time(): number { return this._time; }
+    /**
+     * 色ID(配列)
+     */
+    private _color: number[];
+    get color(): number[] { return this._color; }
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+    /**
+     * コンストラクタ
+     * @param area_name 遠征海域名
+     * @param name 遠征名
+     * @param time 遠征時間
+     * @param color1 色1
+     * @param color2 色2
+     */
+    constructor(areaName: string, name: string, time: number, color1: number, color2: number) {
+        this._areaName = areaName;
+        this._name = name;
+        this._time = time;
+        this._color = [color1, color2];
     }
+};
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+/**
+ * 遠征タスクを表すクラス
+ */
+class ExpeditionTask {
+    /**
+     * 遠征情報
+     */
+    private _expedition: Expedition;
+    get expedition(): Expedition { return this._expedition; }
+    /**
+     * 遠征が始まるタイミングを分単位で指定
+     */
+    private _timing: number;
+    get timing(): number { return this._timing; }
+    /**
+     * 第n艦隊なのかを指定
+     */
+    private _fleetIndex: number;
+    get fleetIndex(): number { return this._fleetIndex; }
+
+    /**
+     * コンストラクタ
+     * @param expedition 遠征情報
+     * @param timing 遠征が始まるタイミング
+     * @param fleet_index 第n艦隊
+     */
+    constructor(expedition: Expedition, timing: number, fleet_index: number) {
+        this._expedition = expedition;
+        this._timing = timing;
+        this._fleetIndex = fleet_index;
     }
+};
 
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
-}
-
+/**
+ * スタートアップ
+ */
 window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    // 遠征リストを初期化
+    var exp1 = new Expedition("鎮守府海域", "長距離練習航海", 30, 1, 4);
+    var exp2 = new Expedition("鎮守府海域", "海上護衛任務",   90, 0, 1);
+    var exp3 = new Expedition("鎮守府海域", "防空射撃演習",   40, 3, 3);
+    var expList = [exp1, exp2, exp3];
+    // 遠征リストと位置情報から、遠征タスクを作成
+    var expTask1 = new ExpeditionTask(expList[1], 90, 3);
+    var expTask2 = new ExpeditionTask(expList[1], 200, 4);
+    var expTaskList = [expTask1, expTask2];
+    // 遠征タスクを表示する
+    var element = document.getElementById('taskList');
+    for (var i = 0; i < expTaskList.length; ++i) {
+        var expTask = expTaskList[i];
+        element.innerText += "・" + expTask.expedition.areaName + "-";
+        element.innerText += expTask.expedition.name + "　";
+        element.innerText += "タイミング：" + expTask.timing + "　";
+        element.innerText += "第" + expTask.fleetIndex + "艦隊\n";
+    }
 };
